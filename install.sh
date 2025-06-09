1204aff842bf9a4d5051def3bf9cccfbd31c2042
nomdedev <martin.nomdedeu.dev@gmail.com>
2025-06-09 20:19:36 -0300

    Organización: mover resultados de test, README de tests y checklists a carpetas dedicadas para mayor orden.

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-python3 -m pip install --upgrade pip
-python3 -m pip install --upgrade -r requirements.txt
-if [ $? -ne 0 ]; then
-    echo "Error durante la instalación de dependencias."
-    exit 1
-fi
-echo "Instalación y actualización de dependencias completada."
